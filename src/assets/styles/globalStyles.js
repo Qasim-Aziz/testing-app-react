0afeb715ebb1ce70fd5e1ae4d627edc9827b3a5d
Devendra Singh <sdev6631@gmail.com>
2021-03-25 16:34:39 +0530

    Add global styles file with variable color

---
+++ src/assets/styles/globalStyles.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+
+export const COLORS = {
+  //   Palette Color
+  palleteLight: '#f9f7f7',
+  palleteLightBlue: '#dbe2ef',
+  palleteBlue: '#3f72af',
+  palleteDarkBlue: '#112d4e',
+
+  // Colors
+  white: '#fff',
+
+  text: '#74708d',
+  textLighten: '#c0bdd0',
+  textDarken: '#615d7c',
+  textDarkenMore: '#514d6a',
+
+  gray: '#d2d9e5',
+  grayLighten: '#eef0f4',
+  grayLightenMore: '#fff',
+  grayLightenMoreMore: '#f9fafc',
+  grayDarken: '#b8beca',
+  grayBorder: '#e4e9f0',
+  grayFill: '#f9f9f9',
+
+  black: '#0e0b20',
+  blackLighten: '#000',
+  blackLightenMore: '#000',
+
+  blue: '#08f',
+  blueDarken: '#0072d6',
+  orange: '#f2a654',
+  yellow: 'yellow',
+
+  menuBg: '#001529',
+  menuBgDarken: '#000c17',
+
+  // Accent colors
+  default: '#acb7bf',
+  primary: '#0190fe',
+  secondary: '#6a7a84',
+  success: '#46be8a',
+  info: '#0887c9',
+  warning: '#f39834',
+  danger: '#fb434a',
+
+  // btn color
+  btnPrimaryColor: '#0b35b3',
+  btnWidth: '200px',
+}
