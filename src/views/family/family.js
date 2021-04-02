b9144b115b8e6c5b392906217c9a3660d51ff6ee
Chinmay-395 <chinmaydali395@gmail.com>
2021-04-02 14:56:28 +0530

    the linting error is muted

--- src/views/family/family.js
+++ src/views/family/family.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-indent */
+/* eslint-disable */
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Button, Card, Layout, Menu, Typography } from 'antd'
